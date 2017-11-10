172203728b2013263c7484dff8969e9f885d05b3
Niels Ladekarl <nielsladekarl@gmail.com>
2017-11-10 21:47:14 +0100

    Started on ViMangler liste

--- src/components/ViMangler.js
+++ src/components/ViMangler.js
@@ -3,7 +3,10 @@ import {
   View,
   StyleSheet,
   ScrollView,
-  Text
+  Text,
+  TouchableOpacity,
+  CheckBox,
+  TextInput
 } from 'react-native';
 import {FontAwesome} from '@expo/vector-icons';
 
@@ -17,40 +20,137 @@ export default class ViManglerScreen extends Component {
     }
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      room: '',
+      value: '',
+      date: ''
+    }
+  }
+
+  renderItems() {
+    let renderItems = [];
+    // change this to use items from DB
+    this.items.forEach(item => {
+      renderItems.push(this._renderItem(item));
+    });
+    return renderItems;
+  }
+
+  items = [
+    {
+      id: 1,
+      room: 'room1',
+      value: 'value1',
+      date: 'date1',
+      checked: false
+    },
+    {
+      id: 2,
+      room: 'room2',
+      value: 'value2',
+      date: 'date3',
+      checked: true
+    },
+    {
+      id: 3,
+      room: 'room3',
+      value: 'value3',
+      date: 'date3',
+      checked: false
+    },
+  ];
+
+  checkBoxClicked(id, value) {
+    this.items.forEach(item => {
+      if (item.id === id) {
+        item.checked = value;
+      }
+    });
+  }
+
+  _renderItem(item) {
+    return (
+      <View key={item.id} style={styles.rowContainer}>
+        <Text style={styles.itemText}>{item.room}</Text>
+        <Text style={styles.itemText}>{item.value}</Text>
+        <Text style={styles.itemText}>{item.date}</Text>
+        <CheckBox value={item.checked}/>
+      </View>
+    );
+  }
+
+  submitItem() {
+  }
+
   render() {
     return (
-      <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.itemContainer}>
-          <View style={styles.rowContainer}>
-            <Text>Hello</Text>
-            <Text>Hello</Text>
+      <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          <View style={styles.itemContainer}>
+            {this.renderItems()}
+            <View style={styles.rowContainer}>
+              <TextInput style={styles.newItemInput} placeholder="Værelse"
+                         placeholderTextColor="#a9a9a9"
+                         value={this.state.room}
+                         onChangeText={(room) => this.setState({room})}/>
+              <TextInput style={styles.newItemInput} placeholder="Ting" value={this.state.value}
+                         placeholderTextColor="#a9a9a9"
+                         onChangeText={(value) => this.setState({value})}/>
+              <TextInput style={styles.newItemInput} placeholder="Dato" value={this.state.date}
+                         placeholderTextColor="#a9a9a9"
+                         onChangeText={(date) => this.setState({date})}/>
+              <TouchableOpacity style={styles.newItemImage} onPress={() => this.submitItem()}>
+                <FontAwesome name="plus-circle" size={20} style={{color: 'black'}}/>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </View>
     )
   };
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
+  },
+  scrollContainer: {
+    height: '100%',
     backgroundColor: '#fff',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     width: '100%',
     alignItems: 'center'
   },
   itemContainer: {
-    flex: 1,
+    width: '100%',
     justifyContent: 'space-between',
-    backgroundColor: '#b4ccff',
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 5,
-    marginBottom: 5
   },
   rowContainer: {
-    flex: 1,
+    backgroundColor: '#eeeeee',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 5,
+    marginBottom: 2,
+    borderRadius: 1,
+    elevation: 1
+  },
+  itemText: {
+    margin: 5,
+    textAlign: 'center'
+  },
+  newItemInput: {
+    height: 40,
+    marginLeft: 5,
+    marginRight: 5,
+    width: '25%'
+  },
+  newItemImage: {
+    margin: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
