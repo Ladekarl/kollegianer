3953528cd66d9f4b74cf4849bd918929233ebdc8
Gioacchino2800 <joakim.s.sie@hotmail.com>
2017-11-14 16:40:23 +0100

    home styling

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -22,31 +22,53 @@ export default class HomeScreen extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.rowContainer}>
-          <View style={styles.columnContainer}>
-            <Image style={styles.image} source={require('../../img/køkkenuge.png')}/>
-            <Text style={styles.text}>Mathias 05</Text>
-          </View>
-          <View style={styles.columnContainer}>
-            <Image style={styles.image} source={require('../../img/sheriff.png')}/>
-            <Text style={styles.text}>Kathrine</Text>
-          </View>
-          <View style={styles.columnContainer}>
-            <Image style={styles.image} source={require('../../img/keep_calm_and_shots.png')}/>
-            <Text style={styles.text}>Mathias 02</Text>
+          <View style={styles.headerContainer}>
+            <Text style={styles.textHeader}>Velkommen til Køkken1700 </Text>
+            <Image style={styles.image} source={require('../../img/baggrund_guld.png')}/>
           </View>
         </View>
         <View style={styles.rowContainer}>
           <View style={styles.columnContainer}>
-            <Image style={styles.image} source={require('../../img/beerpong.png')}/>
-            <Text style={styles.text}>JA</Text>
+            <Image style={styles.image} source={require('../../img/køkkenuge.png')}/>
+            <Text style={styles.text}>Ida</Text>
           </View>
           <View style={styles.columnContainer}>
-            <Image style={styles.image} source={require('../../img/fox.png')}/>
-            <Text style={styles.text}>JA</Text>
+            <Image style={styles.image} source={require('../../img/sheriff.png')}/>
+            <Text style={styles.text}>Niels</Text>
           </View>
           <View style={styles.columnContainer}>
             <Image style={styles.image} source={require('../../img/mvp.png')}/>
-            <Text style={styles.text}>???</Text>
+            <Text style={styles.text}>Niels igen</Text>
+          </View>
+        </View>
+        <View style={styles.rowContainer}>
+          <View style={styles.columnContainer}>
+            <Image style={styles.image} source={require('../../img/keep_calm_and_shots.png')}/>
+            <Text style={styles.text}>Sondrup Larsen</Text>
+          </View>
+          <View style={styles.columnContainer}>
+            <Text style={styles.text}>Beer pong?</Text>
+            <Image style={styles.image} source={require('../../img/beerpong.png')}/>
+            <Text style={styles.text}>Nah fam</Text>
+          </View>
+          <View style={styles.columnContainer}>
+            <Text style={styles.text}>Har vi ræv?</Text>
+            <Image style={styles.image} source={require('../../img/fox.png')}/>
+            <Text style={styles.text}>ofc</Text>
+          </View>
+        </View>
+        <View style={styles.rowContainer}>
+          <View style={styles.columnContainer}>
+            <Text style={styles.text}>Ølregnskab</Text>
+            <Image style={styles.image} source={require('../../img/olregnskab.png')}/>
+          </View>
+          <View style={styles.columnContainer}>
+            <Text style={styles.text}>VIP cub</Text>
+            <Image style={styles.image} source={require('../../img/vip_logo2.png')}/>
+          </View>
+          <View style={styles.columnContainer}>
+            <Text style={styles.text}>Gossip club</Text>
+            <Image style={styles.image} source={require('../../img/gossip_icon.png')}/>
           </View>
         </View>
       </View>
@@ -59,7 +81,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'flex-start'
   },
   columnContainer: {
     backgroundColor: '#eeeeee',
@@ -73,17 +95,36 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 5
   },
+  headerContainer: {
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: 10,
+    padding: 5
+  },
   rowContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    margin: 1
   },
   image: {
-    height: 80,
-    width: 80,
+    height: 60,
+    width: 60,
+    margin: 10,
+  },
+  image: {
+    height: 60,
+    width: 60,
     margin: 10,
   },
   text: {
     textAlign: 'center'
+  },
+  textHeader: {
+    textAlign: 'center',
+    fontWeight: 'bold'
   }
 });
