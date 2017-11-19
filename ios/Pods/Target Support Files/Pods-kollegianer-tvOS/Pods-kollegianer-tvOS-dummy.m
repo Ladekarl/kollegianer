4033f201032dde6bee1a530a50bf781804fe4de9
Niels <nielsladekarl@gmail.com>
2017-11-19 17:36:59 +0100

    Project now compiles for iOS

---
+++ ios/Pods/Target Support Files/Pods-kollegianer-tvOS/Pods-kollegianer-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kollegianer_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_kollegianer_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-kollegianer/Pods-kollegianer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kollegianer : NSObject
+@end
+@implementation PodsDummy_Pods_kollegianer
+@end
