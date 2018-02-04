348fd0c94bd7d7cf71802e7fb96fec889203f0c7
Niels <nielsladekarl@gmail.com>
2018-02-04 20:14:55 +0100

    Fixed ViMangler notification

--- firebase/functions/notifications/ViManglerNotification.js
+++ firebase/functions/notifications/ViManglerNotification.js
@@ -18,7 +18,7 @@ exports.ViManglerNotification = notifyOnWrite('/vimangler/{viManglerUid}', event
 
   return publishNotification(event, (usersSnapshots, committingUid) =>
     getNotificationTokens(usersSnapshots,
-      (userId, user) => user.duty === 'Indkøber' && user.notificationToken && userId !== committingUid
+      (userId, user) => user.duty === 'Indkøber' && user.notificationTokens && String.valueOf(committingUid) !== String.valueOf(userId)
     ), (committingUser) => {
     const viManglerAdded = buildNotification('Der blev tilføjet en ting til Vi Mangler', `${viMangler.item} blev tilføjet af ${committingUser.room}`, 'fcm.VI_MANGLER');
     const viManglerChecked = buildNotification('Der blev købt en ting på Vi Mangler', `${viMangler.item} blev købt af ${committingUser.room}`, 'fcm.VI_MANGLER');
