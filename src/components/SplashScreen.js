1bca393cffb32f2f6e9160018ca2420bcfb2423c
nielsladekarl <nielsladekarl@gmail.com>
2019-02-20 18:21:07 +0100

    fixed error when deleting account and being logged in on multiple devices

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -4,6 +4,7 @@ import LocalStorage from '../storage/LocalStorage';
 import colors from '../shared/colors';
 import {StackActions, NavigationActions} from 'react-navigation';
 import firebase from 'react-native-firebase';
+import Database from '../storage/Database';
 
 const window = Dimensions.get('window');
 const IMAGE_HEIGHT = window.width / 2;
@@ -24,10 +25,8 @@ export default class SplashScreen extends Component {
             LocalStorage.getUser().then(user => {
                 if (user && user.email && user.uid) {
                     if (user.accessToken) {
-                        this._navigateAndReset('mainFlow');
                         this._signInFacebook();
                     } else if (user.password) {
-                        this._navigateAndReset('mainFlow');
                         this._signIn();
                     } else {
                         this._navigateAndReset('Login', true);
@@ -43,8 +42,10 @@ export default class SplashScreen extends Component {
     }
 
     _signIn = () => {
-        LocalStorage.getUser().then(user => {
-            firebase.auth().signInWithEmailAndPassword(user.email, user.password).catch(() => {
+        return LocalStorage.getUser().then(user => {
+            return firebase.auth().signInWithEmailAndPassword(user.email, user.password).then((response) => {
+                this._onSignInSuccess(response, user.password, user.accessToken);
+            }).catch(() => {
                 this._navigateAndReset('Login', true);
             });
         }).catch(error => console.log(error));
@@ -53,10 +54,58 @@ export default class SplashScreen extends Component {
     _signInFacebook = () => {
         LocalStorage.getUser().then(user => {
             const credential = firebase.auth.FacebookAuthProvider.credential(user.accessToken);
-            firebase.auth().signInWithCredential(credential).catch(() => {
+            firebase.auth().signInWithCredential(credential).then((response) => {
+                this._onSignInSuccess(response, user.password, user.accessToken);
+            }).catch(() => {
                 this._navigateAndReset('Login', true);
             });
         }).catch(error => console.log(error));
+    };
+
+    _onSignInSuccess = (response, password, accessToken) => {
+        const user = response.user;
+        Database.getUser(user.uid).then(snapshot => {
+            const dbUser = snapshot.val();
+            if (!dbUser) {
+                return Database.addUser(user).then(() => {
+                    return Database.getUser(user.uid);
+                });
+            }
+            return snapshot;
+        }).then((response) => {
+            const dbUser = response.val();
+            LocalStorage.getFcmToken().then(fcmToken => {
+                if (fcmToken && fcmToken.token) {
+                    if (!dbUser.notificationTokens) {
+                        dbUser.notificationTokens = [];
+                    }
+                    let tokenFound = false;
+                    dbUser.notificationTokens.forEach(t => {
+                        if (t.token && String(t.token).valueOf() == String(fcmToken.token).valueOf()) {
+                            tokenFound = true;
+                        }
+                    });
+                    if (!tokenFound) {
+                        dbUser.notificationTokens.push(fcmToken);
+                    }
+                    Database.updateUser(user.uid, dbUser).catch(error => console.log(error));
+                }
+            });
+            dbUser.uid = user.uid;
+            dbUser.password = password;
+            dbUser.accessToken = accessToken;
+            this._saveUserAndNavigate(dbUser);
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+
+    _saveUserAndNavigate = (dbUser) => {
+        LocalStorage.setUser(dbUser).then(() => {
+            this._navigateAndReset('mainFlow');
+        }).catch(error => {
+            console.log(error);
+        });
     };
 
     _navigateAndReset = (routeName, isNested) => {
