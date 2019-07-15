77b03a869d841494b95132cad7a6185a7b45882d
nielsladekarl <nielsladekarl@gmail.com>
2019-07-15 23:42:00 +0200

    Running on Android

--- android/app/src/main/java/com/kollegianer/MainApplication.java
+++ android/app/src/main/java/com/kollegianer/MainApplication.java
@@ -1,78 +1,78 @@
 package com.kollegianer;
 
 import android.app.Application;
-
-import com.facebook.react.ReactApplication;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import com.rnfs.RNFSPackage;
-import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
-import com.imagepicker.ImagePickerPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.soloader.SoLoader;
+import com.imagepicker.ImagePickerPackage;
+import com.rnfs.RNFSPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import io.github.elyx0.reactnativedocumentpicker.DocumentPickerPackage;
 import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookSdk;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import com.facebook.appevents.AppEventsLogger;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+    private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
-  protected static CallbackManager getCallbackManager() {
-    return mCallbackManager;
-  }
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    protected static CallbackManager getCallbackManager() {
+        return mCallbackManager;
     }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNFetchBlobPackage(),
-            new RNGestureHandlerPackage(),
-            new RNI18nPackage(),
-            new RNFSPackage(),
-            new ReactNativeDocumentPicker(),
-            new ImagePickerPackage(),
-            new RNFirebasePackage(),
-            new RNFirebaseMessagingPackage(),
-            new RNFirebaseAuthPackage(),
-            new RNFirebaseDatabasePackage(),
-            new FBSDKPackage(mCallbackManager)
-      );
-    }
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new RNFetchBlobPackage(),
+                    new RNGestureHandlerPackage(),
+                    new RNI18nPackage(),
+                    new RNFSPackage(),
+                    new DocumentPickerPackage(),
+                    new ImagePickerPackage(),
+                    new RNFirebasePackage(),
+                    new RNFirebaseMessagingPackage(),
+                    new RNFirebaseAuthPackage(),
+                    new RNFirebaseDatabasePackage(),
+                    new RNFirebaseNotificationsPackage(),
+                    new FBSDKPackage(mCallbackManager)
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
 
     @Override
-    protected String getJSMainModuleName() {
-      return "index";
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
 
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    AppEventsLogger.activateApp(this);
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppEventsLogger.activateApp(this);
+        SoLoader.init(this, /* native exopackage */ false);
+    }
 }
