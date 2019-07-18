6582786a61b7d85b5e74002d5578be94a115aa7c
Niels Ladekarl <nielsladekarl@gmail.com>
2019-07-19 01:04:45 +0200

    changed if-condition

--- firebase/functions/src/partyLights.ts
+++ firebase/functions/src/partyLights.ts
@@ -5,10 +5,10 @@ export const PartyLightKiller = functions.pubsub.schedule("every day 05:00")
    .timeZone("Europe/Copenhagen")
    .onRun((context) => {
       return admin.database().ref("events/partymode").set("", (error?) => {
-         if (error && error !== undefined) {
-            console.log("Successfully turned off partylights @", context.timestamp)
-         } else {
+         if (error) {
             console.error(error)
+         } else {
+            console.log("Successfully turned off partylights @", context.timestamp)
          }
       })
    })
